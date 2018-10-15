38541ff5159ce116c3e9b2bd8e53e777ec33d8bb
Joseph Moore <yo.yo.yossarian@gmail.com>
2018-10-15 16:44:31 -0600

    add section 6 example

---
+++ examples/library/tutorial-steps/6-listing-resources/src/main/kotlin/edu/byu/uapi/library/BooksResource.kt
@@ -0,0 +1,83 @@
+package edu.byu.uapi.library
+
+import edu.byu.uapi.kotlin.examples.library.Author
+import edu.byu.uapi.kotlin.examples.library.Book
+import edu.byu.uapi.kotlin.examples.library.Genre
+import edu.byu.uapi.kotlin.examples.library.Library
+import edu.byu.uapi.server.resources.identified.IdentifiedResource
+import edu.byu.uapi.server.resources.identified.fields
+import kotlin.reflect.KClass
+
+class BooksResource : IdentifiedResource<LibraryUser, Long, Book> {
+
+    override val idType: KClass<Long> = Long::class
+
+    override fun loadModel(
+        userContext: LibraryUser,
+        id: Long
+    ): Book? {
+        return Library.getBookByOclc(id)
+    }
+
+    override fun idFromModel(model: Book): Long {
+        return model.oclc
+    }
+
+    override fun canUserViewModel(
+        userContext: LibraryUser,
+        id: Long,
+        model: Book
+    ): Boolean {
+        return true
+    }
+
+    override val responseFields = fields {
+        value(Book::oclc) {
+            key = true
+            displayLabel = "OCLC Control Number"
+            doc = "Control number assigned to this title by the [Online Computer Library Center](www.oclc.org)."
+        }
+        value(Book::title) {
+            displayLabel = "Title"
+            doc = "The main title of the book"
+            modifiable { libraryUser, book, title -> libraryUser.canModifyBooks }
+        }
+        value<Int>("publisher_id") {
+            getValue { book -> book.publisher.publisherId }
+            displayLabel = "Publisher"
+            modifiable { libraryUser, book, value -> libraryUser.canModifyBooks }
+
+            description { book, publisherId -> book.publisher.commonName }
+            longDescription { book, publisherId -> book.publisher.fullName }
+        }
+        value(Book::availableCopies) {
+            isDerived = true
+            displayLabel = "Available Copies"
+        }
+        nullableValue(Book::isbn) {
+            isSystem = true
+            displayLabel = "ISBN"
+            doc = "International Standard Book Number"
+        }
+        valueArray(Book::subtitles) {
+            displayLabel = "Subtitles"
+            doc = "The book's subtitles, if any"
+            modifiable { libraryUser, book, value -> libraryUser.canModifyBooks }
+        }
+        mappedValueArray("author_ids", Book::authors, Author::authorId) {
+            description(Author::name)
+            displayLabel = "Author(s)"
+            modifiable { libraryUser, book, value -> libraryUser.canModifyBooks }
+        }
+        mappedValueArray(Book::genres, Genre::code) {
+            displayLabel = "Genre(s)"
+            description(Genre::name)
+            modifiable { libraryUser, book, value -> libraryUser.canModifyBooks }
+        }
+        value(Book::publishedYear) {
+            displayLabel = "Publication Year"
+            doc = "The year the book was published"
+            modifiable { libraryUser, book, value -> libraryUser.canModifyBooks }
+        }
+    }
+}
