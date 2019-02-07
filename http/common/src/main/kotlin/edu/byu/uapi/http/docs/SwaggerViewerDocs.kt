97df8f7cd86732f0ff1d31273c5df4ec11e21489
Joseph Moore <yo.yo.yossarian@gmail.com>
2019-02-07 11:17:45 -0700

    fix swagger view to be better

--- http/common/src/main/kotlin/edu/byu/uapi/http/docs/SwaggerViewerDocs.kt
+++ http/common/src/main/kotlin/edu/byu/uapi/http/docs/SwaggerViewerDocs.kt
@@ -19,12 +19,26 @@ class SwaggerViewerDocs : DocSource {
             <link rel="stylesheet" href="https://cdn.byu.edu/byu-theme-components/1.x.x/byu-theme-components.min.css" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <script async src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
+            <style>
+              body {
+                display: flex;
+                flex-direction: column;
+              }
+              main {
+                width: 100%;
+                max-width: 1200px;
+                flex-grow: 1;
+              }
+            </style>
           </head>
           <body>
             <byu-header>
-              <h1 slot="site-title">API Documentation</h1>
+              <h1 slot="site-title">UAPI Documentation</h1>
             </byu-header>
-            <rapi-doc spec-url="openapi3.json"></rapi-doc>
+            <main>
+              <rapi-doc spec-url="openapi3.json"></rapi-doc>
+            </main>
+            <byu-footer></byu-footer>
           </body>
         </html>
     """.trimIndent()
