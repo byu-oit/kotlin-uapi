d66ac1249d1577086aed57ad307d9553044e1da0
Joseph Moore <yo.yo.yossarian@gmail.com>
2019-03-04 15:47:25 -0700

    fix issue in jackson schemagen

--- core/schemagen/src/main/kotlin/JacksonSchemaGenerator.kt
+++ core/schemagen/src/main/kotlin/JacksonSchemaGenerator.kt
@@ -3,6 +3,7 @@ package edu.byu.uapi.schemagen
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.treeToValue
 import com.kjetland.jackson.jsonSchema.JsonSchemaGenerator
 import edu.byu.uapi.model.jsonschema07.Schema
@@ -13,6 +14,7 @@ import kotlin.reflect.KClass
 private val defaultObjectMapper by lazy { ObjectMapper().apply {
     findAndRegisterModules()
     registerModule(UAPIModelModule())
+    disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
 } }
 
